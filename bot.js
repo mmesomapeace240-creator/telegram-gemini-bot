4ba8abda3f0bec900086fa71ebb7f33ef792cd9d
mmesomapeace240-creator <mmesomapeace240@gmail.com>
2026-03-17 11:36:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,4 +30,14 @@ bot.on("message", async (msg) => {
         console.log(error);
         bot.sendMessage(chatId, "Something went wrong.");
     }
+});const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
